d7be91c4b3cf29164fad5df88abfa6a551ae6624
Subhrakamal Das <jyb2084.usa@gmail.com>
2021-11-24 21:55:26 +0530

    Day 1

---
+++ Day1/4_String Manipulation.py
@@ -0,0 +1,16 @@
+# topic : String Manipulation and code intelligence;
+
+
+
+
+# Print Statement;
+
+print("Hi")
+print("Hello World \nHello World")  # Breaking line
+
+# Taking those separate string or characters and merging them into one
+print("hello " + "Subhro")  # String Concatenation
+
+
+
+
