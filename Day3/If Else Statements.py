8b6adf24e57ac061e939bfb51f69f7be74746389
Subhrakamal Das <jyb2084.usa@gmail.com>
2021-11-26 00:46:26 +0530

    If Else Statements

---
+++ Day3/If Else Statements.py
@@ -0,0 +1,10 @@
+# Printing if else Statements
+
+
+print("Welcome to the roller Coaster ")
+height = int(input("What's your height?"))
+if height > 120:
+    print("You can ride the coaster")
+else:
+    print("You cannot ride the coaster")
+
