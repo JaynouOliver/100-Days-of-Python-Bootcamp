9dc0c27aa5cce167f475d8cdbb101bbc05d23cf3
Subhrakamal Das <jyb2084.usa@gmail.com>
2021-11-28 23:43:41 +0530

    Day4

---
+++ Day4/3_usingLists.py
@@ -0,0 +1,13 @@
+import random
+
+statesOfAmerica = ["delaware", "Pensylvaania"]
+print(statesOfAmerica[1])
+
+
+# using append function
+statesOfAmerica.append("Hollywood")
+print(statesOfAmerica)
+
+
+
+
